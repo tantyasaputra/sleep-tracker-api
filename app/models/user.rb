a5e7966f07577bedf0cfd6583beca383cdbc96ed
aytnat <tantya.saputra@gmail.com>
2025-04-27 00:36:52 +0700

    Add soft-delete for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
+
+  def destroy
+    update(deleted_at: Time.current)
+  end
+  def deleted?
+    deleted_at.present?
+  end
+
+  # Scope for only active users
+  scope :active, -> { where(deleted_at: nil) }
 end
