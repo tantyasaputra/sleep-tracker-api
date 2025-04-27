fa63aafca2ea2ba025368a3493a334027ea20723
aytnat <tantya.saputra@gmail.com>
2025-04-27 14:52:58 +0700

    Add Basic Auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Basic::ControllerMethods
+  include ExceptionHandler
+
+  before_action :authenticate
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      @current_user = User.find_by(email: username)
+
+      raise AuthenticationError, 'invalid user!' unless @current_user.present?
+      raise AuthenticationError, 'invalid password!' unless @current_user.authenticate(password)
+    end
+  end
 end
