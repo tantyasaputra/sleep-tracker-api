beff38e1aaf6ca147c338165c82b56770a26efc7
aytnat <tantya.saputra@gmail.com>
2025-04-27 18:25:36 +0700

    Add rescue from record not found

--- lib/exception_handler.rb
+++ lib/exception_handler.rb
@@ -9,5 +9,9 @@ module ExceptionHandler
     rescue_from HandledErrors::InvalidParamsError do |e|
       render json: { error: e.message }, status: 422
     end
+
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      render json: { error: e.message }, status: 404
+    end
   end
 end
