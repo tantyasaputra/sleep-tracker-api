64b9f39ded78d0f5c7fd3452bd89c0d926f4d232
aytnat <tantya.saputra@gmail.com>
2025-04-27 16:33:52 +0700

    Add routes GET users#index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,32 @@
 class UsersController < ApplicationController
+
+  def index
+    @page = (params[:page] || 1).to_i
+    @per_page = (params[:per_page] || 10).to_i
+
+    # Calculate offset
+    offset = (@page - 1) * @per_page
+    @total_count = User.active.where.not(id: @current_user.id).count
+    @total_pages = (@total_count.to_f / @per_page).ceil
+
+    @users = User.active
+                 .where.not(id: @current_user.id)
+                 .select(:id, :email)
+                 .order(created_at: :desc)
+                 .limit(@per_page)
+                 .offset(offset)
+
+    render json: {
+      data: @users.map { |user| { id: user.id, email: user.email } },
+      meta: {
+        current_page: @page,
+        per_page: @per_page,
+        total_pages: @total_pages,
+        total_count: @total_count
+      }
+    }
+  end
+
   def profiles
     render json: { email: @current_user.email, followers: @current_user.followers.size, following: @current_user.following.size }
   end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  resources :users, only: [] do
+  resources :users, only: [:index] do
     collection do
       get "profiles", to: "users#profiles"
     end
