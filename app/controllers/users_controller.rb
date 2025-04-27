700446decc1cee32ff97635bc95e7d71ef356006
aytnat <tantya.saputra@gmail.com>
2025-04-27 23:38:22 +0700

    Refactor get user list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,29 @@
 class UsersController < ApplicationController
   def index
-    @page = (params[:page] || 1).to_i
-    @per_page = (params[:per_page] || 10).to_i
+    page = params[:page].present? ? params[:page].to_i : 1
+    per_page = params[:per_page].present? ? params[:per_page] : 10
 
     # Calculate offset
-    offset = (@page - 1) * @per_page
-    @total_count = User.active.where.not(id: @current_user.id).count
-    @total_pages = (@total_count.to_f / @per_page).ceil
+    offset = (page - 1) * per_page
 
-    @users = User.active
-                 .where.not(id: @current_user.id)
-                 .select(:id, :email)
-                 .order(created_at: :desc)
-                 .limit(@per_page)
-                 .offset(offset)
+    users_query = User.active.where.not(id: @current_user.id)
+
+    total_count = users_query.count
+    total_pages = total_count.zero? ? 0 : (total_count.to_f / per_page).ceil
+
+    users = users_query
+              .select(:id, :email)
+              .order(created_at: :desc)
+              .limit(per_page)
+              .offset(offset)
 
     render json: {
-      data: @users.map { |user| { id: user.id, email: user.email } },
+      data: users.as_json(only: [:id, :email]),
       meta: {
-        current_page: @page,
-        per_page: @per_page,
-        total_pages: @total_pages,
-        total_count: @total_count
+        current_page: page,
+        per_page: per_page,
+        total_pages: total_pages,
+        total_count: total_count
       }
     }
   end
