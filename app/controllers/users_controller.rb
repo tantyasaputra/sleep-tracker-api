b3031dcdd322b3815046816fadf5e0c311e0b890
aytnat <tantya.saputra@gmail.com>
2025-04-27 15:49:30 +0700

    Add followers & following in profiles response

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
   def profiles
-    render json: { email: @current_user.email }
+    render json: { email: @current_user.email, followers: @current_user.followers.size, following: @current_user.following.size }
   end
 end
