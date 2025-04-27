119935ffb5ab205ca8867ad33c2edafe4965bab9
aytnat <tantya.saputra@gmail.com>
2025-04-27 12:26:18 +0700

    Spec: Add test case for user follows logic

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -38,4 +38,88 @@ RSpec.describe User, type: :model do
       expect { user.destroy }.not_to change(User, :count)
     end
   end
+
+  describe "following relationships" do
+    let(:user) { create(:user, email: 'user@example.com') }
+    let(:other_user) { create(:user, email: 'other@example.com') }
+
+    it "can follow another user" do
+      expect {
+        user.follow!(other_user)
+      }.to change { user.following.count }.by(1)
+
+      expect(user.following?(other_user)).to be true
+      expect(other_user.followers).to include(user)
+    end
+
+    it "can unfollow a followed user" do
+      user.follow!(other_user)
+      expect(user.following?(other_user)).to be true
+
+      expect {
+        user.unfollow!(other_user)
+      }.to change { user.following.count }.by(-1)
+
+      expect(user.following?(other_user)).to be false
+    end
+
+    it "can re-follow another user" do
+      user.follow!(other_user)
+      user.unfollow!(other_user)
+
+      expect {
+        user.follow!(other_user)
+      }.to change { user.following.count }.by(1)
+
+      expect(user.following?(other_user)).to be true
+      expect(other_user.followers).to include(user)
+    end
+
+    it "does not allow following self" do
+      expect {
+        user.follow!(user)
+      }.not_to change { user.following.count }
+
+      expect(user.following?(user)).to be false
+    end
+
+    it "does not duplicate follows" do
+      user.follow!(other_user)
+      expect {
+        user.follow!(other_user)
+      }.not_to change { user.following.count }
+    end
+  end
+
+  describe "#soft_delete_follows" do
+    let(:user) { create(:user, email: 'user@example.com') }
+    let(:other_user) { create(:user, email: 'other@example.com') }
+
+    before do
+      user.follow!(other_user)
+    end
+
+    it "soft deletes follow relationships when the user is soft deleted" do
+      follow = Follow.find_by(follower_id: user.id, followed_id: other_user.id)
+      expect(follow.deleted_at).to be_nil
+
+      # Soft delete the follower
+      user.destroy
+
+      follow.reload
+      expect(follow.deleted_at).not_to be_nil
+    end
+
+    it "does not affect other follows when a user is soft deleted" do
+      another_user = create(:user, email: 'another_user@example.com')
+      other_user.follow!(another_user)
+
+      # Soft delete the original follower
+      user.destroy
+
+      # Check that the new follow is not deleted
+      follow = Follow.find_by(follower_id: other_user.id, followed_id: another_user.id)
+      expect(follow.deleted_at).to be_nil
+    end
+  end
 end
