f5ded674cd219cf880cdfe5387e2b96c7bec9ef1
aytnat <tantya.saputra@gmail.com>
2025-04-27 15:34:12 +0700

    Fix authenticate! logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,25 @@ class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Basic::ControllerMethods
   include ExceptionHandler
 
-  before_action :authenticate
+  before_action :authenticate!
 
   private
 
-  def authenticate
+  def authenticate!
     authenticate_or_request_with_http_basic do |username, password|
       @current_user = User.find_by(email: username)
 
-      raise AuthenticationError, 'invalid user!' unless @current_user.present?
-      raise AuthenticationError, 'invalid password!' unless @current_user.authenticate(password)
+      unless @current_user.present?
+        raise AuthenticationError, 'invalid user!'
+        return false
+      end
+
+      unless @current_user.authenticate(password)
+        raise AuthenticationError, 'invalid password!'
+        return false
+      end
+
+      return true
     end
   end
 end
