163399ec592c4fbae58fa3c60a46996eba68b5cd
aytnat <tantya.saputra@gmail.com>
2025-04-30 12:00:55 +0700

    Refactor pagination on sleep logs index

--- app/controllers/sleep_logs_controller.rb
+++ app/controllers/sleep_logs_controller.rb
@@ -1,4 +1,6 @@
 class SleepLogsController < ApplicationController
+  include Pagy::Backend
+  include Pagy::Frontend
   def clock_in
     sleep_log = SleepLog.clock_in(@current_user)
     render json: { message: "successfully clocked in", sleep_log: sleep_log }, status: :created
@@ -14,35 +16,23 @@ class SleepLogsController < ApplicationController
   end
 
   def following
-    page = params[:page].to_i
-    page = 1 if page <= 0
-
     per_page = params[:per_page].to_i
     per_page = 10 if per_page <= 0
 
     duration_days = params[:duration_days].to_i
     duration_days = 7 if duration_days <= 0
 
-    offset = (page - 1) * per_page
-    previous_date = duration_days.days.ago
-    time_range = previous_date..Time.current
+    time_range = duration_days.days.ago..Time.current
 
     following_ids = @current_user.following.pluck(:id)
-
-    sleep_logs_query = SleepLog.where(user_id: following_ids)
+    sleep_logs = SleepLog.where(user_id: following_ids)
                                .where(sleep_at: time_range)
                                .where.not(wake_at: nil)
 
-    total_count = sleep_logs_query.count
-    total_pages = (total_count.to_f / per_page).ceil
-
-    sleep_logs = sleep_logs_query
-                   .order(duration: :desc) # optional: add ordering if needed
-                   .limit(per_page)
-                   .offset(offset)
+    @pagy, @records = pagy(sleep_logs, limit: per_page, overflow: :empty_page)
 
     render json: {
-      data: sleep_logs.map do |sleep_log|
+      data: @records.map do |sleep_log|
         {
           id: sleep_log.id,
           sleep_at: sleep_log.sleep_at,
@@ -53,10 +43,10 @@ class SleepLogsController < ApplicationController
         }
       end,
       meta: {
-        current_page: page,
-        per_page: per_page,
-        total_pages: total_pages,
-        total_count: total_count
+        current_page: @pagy.page,
+        per_page: @pagy.limit,
+        total_pages: @pagy.pages,
+        total_count: @pagy.count
       }
     }
   end
